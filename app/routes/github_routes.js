066cd4ce9a24b5660950b86dc2330777f3fc652a
Samkit Jain <samkitjain5795@gmail.com>
2017-11-17 16:38:11 +0530

    bug fix

--- app/routes/github_routes.js
+++ app/routes/github_routes.js
@@ -1,26 +1,25 @@
 const fetch = require('node-fetch');
 
-const grabContent = url => fetch(url)
-	.then(res => res.text())
-	.then(html => {
-		//process html
-	});
+// list all repositories by a user
+const get_repositories = url => fetch(url)
+	.then(response => response.json());
 
 module.exports = function(app, db) {
 	app.get('/users/:name', (req, res) => {
-		// dictionary to store all the fields (data) of the user
-		let user_info = {
-			'stars': 0,
-			'forks': 0,
-			'commits': 0,
-			'repos': [],
-		};
-
 		fetch('https://api.github.com/users/' + req.params.name + '?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88')
 			.then(response => {
 				return response.json();
 			})
 			.then(user_data => {
+				// dictionary to store all the fields (data) of the user
+				let user_info = {
+					'stars': 0,
+					'forks': 0,
+					'commits': 0,
+					'repos': [],
+				};
+				let repo_urls = [];
+
 				// saving required fields in user_info
 				user_info['public_repos'] = user_data['public_repos'];
 				user_info['avatar_url'] = user_data['avatar_url'];
@@ -31,40 +30,40 @@ module.exports = function(app, db) {
 				user_info['name'] = user_data['name'];
 				user_info['bio'] = user_data['bio'];
 
-				// get all repos
-				return fetch('https://api.github.com/users/' + req.params.name + '/repos?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88&type=all');
-			})
-			.then(response => {
-				return response.json();
-			})
-			.then(repo_data => {
-				for(let i = 0; i < user_info['public_repos']; i++) {
-					if(repo_data[i]['owner']['login'] === user_info['login']) {
-						user_info['stars'] += repo_data[i]['stargazers_count'];
-						user_info['forks'] += repo_data[i]['forks_count'];
-					}
-					
-					user_info['repos'].push({
-						'html_url': repo_data[i]['html_url'],
-						'full_name': repo_data[i]['full_name'],
-						'stargazers_count': repo_data[i]['stargazers_count'],
-						'forks_count': repo_data[i]['forks_count'],
-						'watchers_count': repo_data[i]['watchers_count'],
-						// 'commits': repo_data[i]['commits'],
-						// 'all_commits': repo_data[i]['all_commits'],
-					});
+				repo_url = 'https://api.github.com/users/' + req.params.name + '/repos?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88&type=all&per_page=50&page=';
+
+				for(let page_count = 1; page_count <= Math.floor(user_info['public_repos'] / 50) + 1; page_count++) {
+					repo_urls.push(repo_url + page_count)
 				}
 
-				console.log(user_info);
-				res.json(user_info);
+				Promise
+					.all(repo_urls.map(get_repositories))
+					.then(values => {
+						values.forEach((repo_data) => {
+							for(let i = 0; i < repo_data.length; i++) {
+								if(repo_data[i]['owner']['login'] === user_info['login']) {
+									user_info['stars'] += repo_data[i]['stargazers_count'];
+									user_info['forks'] += repo_data[i]['forks_count'];
+								}
+								
+								user_info['repos'].push({
+									'html_url': repo_data[i]['html_url'],
+									'full_name': repo_data[i]['full_name'],
+									'stargazers_count': repo_data[i]['stargazers_count'],
+									'forks_count': repo_data[i]['forks_count'],
+									'watchers_count': repo_data[i]['watchers_count'],
+									// 'commits': repo_data[i]['commits'],
+									// 'all_commits': repo_data[i]['all_commits'],
+								});
+							}
+						});
+
+						res.json(user_info)
+					});
 			})
 			.catch(error => {
 				console.log(error);
 				res.json({'error': error});
 			});
-
-			//Promise
-			//	.all(urls.map(grabContent))
-			//	.then(() => console.log('urlsksjksj'));
 	});
 };
