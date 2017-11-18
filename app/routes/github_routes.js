37b4db01f0cd3dc9e2cd99e34bfbffa232958264
Samkit Jain <samkitjain5795@gmail.com>
2017-11-18 16:03:01 +0530

    minor changes

--- app/routes/github_routes.js
+++ app/routes/github_routes.js
@@ -4,8 +4,13 @@ const fetch = require('node-fetch');
 const get_repositories = url => fetch(url)
 	.then(response => response.json());
 
+// details of forked repos
+const fork_details = url => fetch(url)
+	.then(response => response.json());
+
 module.exports = function(app, db) {
 	app.get('/users/:name', (req, res) => {
+		// get basic user information
 		fetch('https://api.github.com/users/' + req.params.name + '?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88')
 			.then(response => {
 				return response.json();
@@ -13,11 +18,12 @@ module.exports = function(app, db) {
 			.then(user_data => {
 				// dictionary to store all the fields (data) of the user
 				let user_info = {
-					'stars': 0,
-					'forks': 0,
 					'commits': 0,
 					'repos': [],
+					'stars': 0,
+					'forks': 0,
 				};
+				// array to store repository URLs
 				let repo_urls = [];
 
 				// saving required fields in user_info
@@ -30,36 +36,67 @@ module.exports = function(app, db) {
 				user_info['name'] = user_data['name'];
 				user_info['bio'] = user_data['bio'];
 
-				repo_url = 'https://api.github.com/users/' + req.params.name + '/repos?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88&type=all&per_page=50&page=';
+				repo_url = 'https://api.github.com/users/' + req.params.name + '/repos?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88&type=all&per_page=500&page=';
 
-				for(let page_count = 1; page_count <= Math.floor(user_info['public_repos'] / 50) + 1; page_count++) {
+				for(let page_count = 1; page_count <= Math.floor(user_info['public_repos'] / 500) + 1; page_count++) {
 					repo_urls.push(repo_url + page_count)
 				}
 
 				Promise
 					.all(repo_urls.map(get_repositories))
 					.then(values => {
+						let forks_urls = [];
+
 						values.forEach((repo_data) => {
 							for(let i = 0; i < repo_data.length; i++) {
-								if(repo_data[i]['owner']['login'] === user_info['login']) {
-									user_info['stars'] += repo_data[i]['stargazers_count'];
-									user_info['forks'] += repo_data[i]['forks_count'];
+								if(repo_data[i]['fork']) {
+									forks_urls.push(repo_data[i]['url'] + '?client_id=306bffb6acf1e4b78303&client_secret=64f16f44d1346f04b72e6c9cb3f60e727b400c88');
+								} else {
+									user_info['repos'].push({
+										'stargazers_count': repo_data[i]['stargazers_count'],
+										'contributors_url': repo_data[i]['contributors_url'],
+										'watchers_count': repo_data[i]['watchers_count'],
+										'owner_login': repo_data[i]['owner']['login'],
+										'forks_count': repo_data[i]['forks_count'],
+										'full_name': repo_data[i]['full_name'],
+										'html_url': repo_data[i]['html_url'],
+										'all_commits': 0,
+										'commits': 0,
+									});
 								}
-								
-								user_info['repos'].push({
-									'html_url': repo_data[i]['html_url'],
-									'full_name': repo_data[i]['full_name'],
-									'stargazers_count': repo_data[i]['stargazers_count'],
-									'forks_count': repo_data[i]['forks_count'],
-									'watchers_count': repo_data[i]['watchers_count'],
-									// 'commits': repo_data[i]['commits'],
-									// 'all_commits': repo_data[i]['all_commits'],
-								});
 							}
 						});
 
-						res.json(user_info)
+						return Promise.all(forks_urls.map(fork_details));
+					})
+					.then(forks => {
+						forks.forEach((fork_data) => {
+							user_info['repos'].push({
+								'stargazers_count': fork_data['parent']['stargazers_count'],
+								'contributors_url': fork_data['parent']['contributors_url'],
+								'watchers_count': fork_data['parent']['watchers_count'],
+								'owner_login': fork_data['parent']['owner']['login'],
+								'forks_count': fork_data['parent']['forks_count'],
+								'full_name': fork_data['parent']['full_name'],
+								'html_url': fork_data['parent']['html_url'],
+								'all_commits': 0,
+								'commits': 0,
+							});
+						});
+
+						// counting number of forks and stars on repos by user
+						for(let i = 0; i < user_info['repos'].length; i++) {
+							if(user_info['login'] === user_info['repos'][i]['owner_login']) {
+								user_info['stars'] += user_info['repos'][i]['stargazers_count'];
+								user_info['forks'] += user_info['repos'][i]['forks_count'];
+							}
+						}
+
+						res.json(user_info);
 					});
+					// .then(() => {
+					// 	hjh
+					// });
 			})
 			.catch(error => {
 				console.log(error);
