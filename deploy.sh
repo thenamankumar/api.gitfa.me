01bc5cbe5b6d7cfbffc8e9064cdf18af10959b5c
Naman Kumar <naman@outlook.in>
2017-11-21 18:58:57 +0530

    Removed scp

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf /var/www/html/gitfame-backend/*;"
-scp -o StrictHostKeyChecking=no -i key.pem -r ./* $USER@$SERVER:/var/www/html/gitfame-backend
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "pm2 start /var/www/html/gitfame-backend/server.js;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd /var/www/html/gitfame-backend; git pull origin master; npm i; pm2 restart server.js;"
