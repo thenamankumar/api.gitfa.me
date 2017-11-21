b41becbd106a3563ded584b8562bb12bb323128b
Naman Kumar <naman@outlook.in>
2017-11-21 18:47:00 +0530

    Removed pm2 stop

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "pm2 stop server; rm -rf /var/www/html/gitfame-backend/*;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf /var/www/html/gitfame-backend/*;"
 scp -o StrictHostKeyChecking=no -i key.pem -r ./* $USER@$SERVER:/var/www/html/gitfame-backend
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "pm2 start /var/www/html/gitfame-backend/server.js;"
