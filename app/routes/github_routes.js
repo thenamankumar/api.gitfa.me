311dae655f31b30c7f60a42e5c518256ceb2aa90
Samkit Jain <samkitjain5795@gmail.com>
2017-11-26 16:04:29 +0530

    use of fragments

--- app/routes/github_routes.js
+++ app/routes/github_routes.js
@@ -1,90 +1,69 @@
 const fetch = require('node-fetch');
 
-// details repositories by the user
-const get_repositories = url => fetch(url)
-	.then(response => response.json());
-
-// details of forked repositories by the user
-const fork_details = url => fetch(url)
-	.then(response => response.json());
-
 // return GraphQL query
 function get_payload(user_name) {
 	return {
-		"query": `{
-			user(login: "samkit-jain") {
-				avatarUrl
-				bio
-				login
-				name
-				url
-				followers {
-					totalCount
-				}
-				following {
-					totalCount
-				}
-				repositories(first: 100, orderBy: {field: NAME, direction: ASC}) {
-					totalCount
-					pageInfo {
-						hasNextPage
-						endCursor
+		"query": `
+			{
+				user(login: "samkit-jain") {
+					avatarUrl
+					bio
+					login
+					name
+					url
+					followers {
+						totalCount
 					}
-					edges {
-						node {
-							... on Repository {
-								name
-								isFork
-								owner {
-									login
-								}
-								stargazers {
-									totalCount
-								}
-								watchers {
-									totalCount
-								}
-								forks {
-									totalCount
-								}
-								parent {
-									owner {
-										login
+					following {
+						totalCount
+					}
+					repositories(first: 100, orderBy: {field: NAME,direction: ASC}) {
+						totalCount
+						pageInfo {
+							hasNextPage
+							endCursor
+						}
+						edges {
+							node {
+								... on Repository {
+									isFork
+									parent {
+										... repoStats
 									}
-									stargazers {
-										totalCount
-									}
-									watchers {
-										totalCount
-									}
-									forks {
-										totalCount
-									}
-									ref(qualifiedName: "master") {
-										target {
-											... on Commit {
-												history {
-													totalCount
-												}
-											}
-										}
-									}
-								}
-							}
-							ref(qualifiedName: "master") {
-								target {
-									... on Commit {
-										history {
-											totalCount
-										}
-									}
+									... repoStats
 								}
 							}
 						}
 					}
 				}
 			}
-		}`
+
+			fragment repoStats on Repository {
+				nameWithOwner
+				url
+				owner {
+					login
+				}
+				stargazers {
+					totalCount
+				}
+				watchers {
+					totalCount
+				}
+				forks {
+					totalCount
+				}
+				ref(qualifiedName: "master") {
+					target {
+						... on Commit {
+							history {
+								totalCount
+							}
+						}
+					}
+				}
+			}
+		`
 	}
 }
 
@@ -100,10 +79,9 @@ module.exports = function(app, db) {
 			}
 		})
 			.then(response => {
-				if(response.ok)
-					return response.json();
-				
-				throw new Error("Not found");
+				if(response.ok) return response.json();
+
+				throw new Error('error');
 			})
 			.then(user_data => {
 				let user_info = {
@@ -119,6 +97,25 @@ module.exports = function(app, db) {
 				user_info['login'] = user_data['login'];
 				user_info['name'] = user_data['name'];
 				user_info['bio'] = user_data['bio'];
+
+				user_info['repos'] = [];
+
+				user_data['repositories']['edges'].forEach((repo_node) => {
+					repo_node = repo_node['node'];
+
+					if(repo_node['isFork'])	repo_node = repo_node['parent'];
+
+					user_info['repos'].push({
+						'full_name': repo_node['nameWithOwner'],
+						'stargazers_count': repo_node['stargazers']['totalCount'],
+						'watchers_count': repo_node['watchers']['totalCount'],
+						'owner_login': repo_node['owner']['login'],
+						'forks_count': repo_node['forks']['totalCount'],
+						'html_url': repo_node['url'],
+						'all_commits': repo_node['ref']['target']['history']['totalCount'],
+						'commits': repo_node['nameWithOwner'],
+					});
+				})
 
 				res.json(user_info);
 			})
