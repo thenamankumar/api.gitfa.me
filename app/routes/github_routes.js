fcc3999208f100c853528c885301496c2c188a2b
Samkit Jain <samkitjain5795@gmail.com>
2017-12-03 19:59:29 +0530

    made POST

--- app/routes/github_routes.js
+++ app/routes/github_routes.js
@@ -117,7 +117,7 @@ function reposPayload(username, id, endCursor) {
 function fetchData(req, res) {
   return fetch('https://api.github.com/graphql', {
     method: 'POST',
-    body: JSON.stringify(userPayload(req.params.name, true, null)),
+    body: JSON.stringify(userPayload(req.body.name, true, null)),
     headers: {
       'Authorization': 'bearer ' + process.env.GIT_TOKEN,
       'Content-Type': 'application/json'
@@ -276,67 +276,108 @@ function fetchData(req, res) {
 }
 
 module.exports = function (app, db) {
-  app.get('/users/:name', (req, res) => {
-    const idDict = {'_id': req.params.name};
+  app.post('/', (req, res) => {
+    if(req.body.name === undefined || req.body.name === null || req.body.latest === undefined || req.body.latest === null) {
+      return res.json({'success': false, 'message': 'parameters missing'});
+    } else {
+      const idDict = {'_id': req.body.name};
 
-    db.collection('stats').findOne(idDict, (err, item) => {
-      if(err) {
-        return res.json({'success': false, 'message': err.message});
-      }
+      if(req.body.latest === true) {
+        // if 24 hours passed
+        // get new data and save
+        // else return whatever's in DB
 
-      if(item === null) {
-        // ID not present in DB
-        fetchData(req, res)
-          .then((finalData) => {
-            // save to db
-            finalData['_id'] = finalData['login'];
+        db.collection('stats').findOne(idDict, (err, item) => {
+          if(err) {
+            return res.json({'success': false, 'message': err.message});
+          } else if(item === null) {
+            // ID not present in DB
+            fetchData(req, res)
+              .then((finalData) => {
+                // save to db
+                finalData['_id'] = finalData['login'];
 
-            db.collection('stats').insert(finalData, (err, result) => {
-              if (err) {
-                console.log('Insert failed. Error - ' + err.message); 
-              } else {
-                console.log('Inserted. ID: ' + result.ops[0]['_id']);
-              }
-            });
+                db.collection('stats').insert(finalData, (err, result) => {
+                  if (err) {
+                    console.log('Insert failed. Error - ' + err.message); 
+                  } else {
+                    console.log('Inserted. ID: ' + result.ops[0]['_id']);
+                  }
+                });
 
-            return res.json(finalData);
-          })
-          .catch(error => {
-            return res.json({'success': false, 'message': error.message});
-          });
-      } else {
-        // id present in DB
-
-        var staDate = new Date(item['time']); // time when entry was saved
-        var curDate = new Date();  // current time
-
-        if((curDate.getTime() - staDate.getTime()) > 86400000) {
-          // more than 24 hours old data
-          // get new data and update
-
-          fetchData(req, res)
-            .then((finalData) => {
-              // update to db
-              finalData['_id'] = finalData['login'];
-
-              db.collection('stats').update(idDict, finalData, (err, result) => {
-                if (err) {
-                  console.log('Update failed. Error - ' + err.message); 
-                } else {
-                  console.log('Updated. ID: ' + finalData['_id']);
-                }
+                return res.json(finalData);
+              })
+              .catch(error => {
+                return res.json({'success': false, 'message': error.message});
               });
+          } else {
+            // id present in DB
 
-              return res.json(finalData);
-            })
-            .catch(error => {
-              return res.json({'success': false, 'message': error.message});
-            });
-        } else {
-          // data is fresh
-          return res.json(item);
-        }
+            var staDate = new Date(item['time']); // time when entry was saved
+            var curDate = new Date();  // current time
+
+            if((curDate.getTime() - staDate.getTime()) > 86400000) {
+              // more than 24 hours old data
+              // get new data and update
+
+              fetchData(req, res)
+                .then((finalData) => {
+                  // update to db
+                  finalData['_id'] = finalData['login'];
+
+                  db.collection('stats').update(idDict, finalData, (err, result) => {
+                    if (err) {
+                      console.log('Update failed. Error - ' + err.message); 
+                    } else {
+                      console.log('Updated. ID: ' + finalData['_id']);
+                    }
+                  });
+
+                  return res.json(finalData);
+                })
+                .catch(error => {
+                  return res.json({'success': false, 'message': error.message});
+                });
+            } else {
+              // data is fresh
+              return res.json(item);
+            }
+          }
+        });
+      } else {
+        // return whatever's in db
+        // if nothing in db then get from GitHub and save
+
+        db.collection('stats').findOne(idDict, (err, item) => {
+          if(err) {
+            return res.json({'success': false, 'message': err.message});
+          } else if(item === null) {
+            // ID not present in DB
+            fetchData(req, res)
+              .then((finalData) => {
+                // save to db
+                finalData['_id'] = finalData['login'];
+
+                db.collection('stats').insert(finalData, (err, result) => {
+                  if (err) {
+                    console.log('Insert failed. Error - ' + err.message); 
+                  } else {
+                    console.log('Inserted. ID: ' + result.ops[0]['_id']);
+                  }
+                });
+
+                return res.json(finalData);
+              })
+              .catch(error => {
+                return res.json({'success': false, 'message': error.message});
+              });
+          } else {
+            // id present in DB
+
+            return res.json(item);
+          }
+        });
       }
-    });
+    }
   });
 };
