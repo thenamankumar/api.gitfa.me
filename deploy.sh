dc73d4f8c6a8c461a751367232869da5e0da1e76
hereisnaman <naman@outlook.in>
2018-01-30 11:46:13 +0530

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 chmod 600 key.pem
 npm i
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf /var/www/html/gitfame-backend/*;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "pm2 stop gitfame-backend; rm -rf /var/www/html/gitfame-backend/*;"
 scp -o StrictHostKeyChecking=no -i key.pem -r ./* $USER@$SERVER:/var/www/html/gitfame-backend
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "pm2 restart gitfame-backend;"
