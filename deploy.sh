b5983b8abe53f5fc811c159e58fde5c0def9cf6b
hereisnaman <naman@outlook.in>
2018-04-10 20:18:35 +0530

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd /var/www/html/gitfame-backend; git reset --hard HEAD; git pull origin master; npm i; pm2 restart gitfame-backend;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd /var/www/html/backend; git reset --hard HEAD; git pull origin master; npm i; pm2 restart server;"
