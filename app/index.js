3b16d18ff7bf440189354257c2fe75191b462fa9
Naman Kumar <naman@outlook.in>
2018-04-24 22:57:09 +0530

    app structure

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-const { GraphQLServer } = require('graphql-yoga');
+const {GraphQLServer} = require('graphql-yoga');
+const Query = require('./resolvers/query/');
 
-const resolvers = {};
+const resolvers = {
+  Query,
+};
 
 const server = new GraphQLServer({
   typeDefs: 'app/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req
+  }),
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
