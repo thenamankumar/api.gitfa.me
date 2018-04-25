7e24e5ae0b1d746084006f9024f2fc0ceb080d1f
Naman Kumar <naman@outlook.in>
2018-04-25 11:55:09 +0530

    added prisma

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
+const path = require('path');
+require('dotenv').config({path: path.resolve(process.cwd(), `env/${process.env === 'production' ? 'prod' : 'dev'}.env`)});
 const {GraphQLServer} = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 const Query = require('./resolvers/query/');
 
 const resolvers = {
@@ -9,7 +12,13 @@ const server = new GraphQLServer({
   typeDefs: 'app/schema.graphql',
   resolvers,
   context: req => ({
-    ...req
+    ...req,
+    db: new Prisma({
+      typeDefs: 'app/database/api.graphql',
+      endpoint: `http://localhost:4466/`,
+      secret: process.env.DB_API_SECRET,
+      debug: true,
+    }),
   }),
 });
 
