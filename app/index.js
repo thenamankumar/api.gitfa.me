ccda1223bf3e86ad3a3161315aee6459e4daba3d
Naman Kumar <naman@outlook.in>
2018-04-25 20:30:09 +0530

    Added Sentry

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const { GraphQLServer } = require('@fabien0102/graphql-yoga');
 const { Prisma } = require('prisma-binding');
 const { ApolloEngine } = require('apollo-engine');
 const compression = require('compression');
+const Raven = require('raven');
 const resolvers = require('./resolvers/');
 
 // server port
@@ -11,7 +12,7 @@ const port = process.env.port || 4000;
 
 // create a new graphql server
 const server = new GraphQLServer({
-  typeDefs: 'app/schema.graphql',
+  typeDefs: 'app/schema.graphql', // application api schema
   resolvers,
   context: req => ({
     ...req,
@@ -33,23 +34,28 @@ const serverOptions = {
 // enable gzip compression
 server.express.use(compression());
 
-if (process.env.APOLLO_ENGINE_KEY) {
-  // create a new apollo engine instance
-  const engine = new ApolloEngine({
-    apiKey: process.env.APOLLO_ENGINE_KEY, // apollo engine api key
-  });
+if (process.env.NODE_ENV === 'production') {
+  // initiate sentry
+  Raven.config(process.env.SENTRY_KEY).install();
 
-  const httpServer = server.createHttpServer(serverOptions); // create a http server from graphql server instance
+  if (process.env.APOLLO_ENGINE_KEY) {
+    // create a new apollo engine instance
+    const engine = new ApolloEngine({
+      apiKey: process.env.APOLLO_ENGINE_KEY, // apollo engine api key
+    });
 
-  // start server with apollo engine
-  engine.listen(
-    {
-      port,
-      httpServer,
-      graphqlPaths: ['/'],
-    },
-    () => console.log(`Server with Apollo Engine is running on http://localhost:${port}`),
-  );
+    const httpServer = server.createHttpServer(serverOptions); // create a http server from graphql server instance
+
+    // start server with apollo engine
+    engine.listen(
+      {
+        port,
+        httpServer,
+        graphqlPaths: ['/'],
+      },
+      () => console.log(`Server with Apollo Engine is running on http://localhost:${port}`),
+    );
+  }
 } else {
   // start server without apollo engine
   server.start(
