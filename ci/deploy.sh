bdc943bceb4743043bef627f86756413c4443bce
Naman Kumar <naman@outlook.in>
2018-04-26 15:40:02 +0530

    update pm2 env on restart

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 chmod 600 key.pem
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd ~/api/; git pull origin master; yarn; yarn prisma; pm2 restart api.sharecake.io;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd ~/api/; git pull origin master; yarn; yarn prisma; pm2 restart api.sharecake.io --update-env;"
