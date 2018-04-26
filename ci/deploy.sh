04eaf60d928c69a2a60a0854111248f967091be5
Naman Kumar <naman@outlook.in>
2018-04-26 14:17:48 +0530

    auto deployment

---
+++ ci/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+chmod 600 key.pem
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cd ~/api/; git pull origin master; nvm use; yarn; yarn prisma; pm2 restart api.sharecake.io;"
